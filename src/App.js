286abd71ff9ab74ac121c9f1e7da58e41585fcbf
h-miiaah <haim.miiaah5@gmail.com>
2021-02-28 14:39:45 -0500

    adjusted fetch request link in componentDidMount to check requests from the backend data.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
   componentDidMount(){
     fetch('http://localhost:3001/api/v1/boxes')
     .then(response => response.json())
-    .then(data => console.log(data[0].items))
+    .then(data => console.log(data))
   }
 
   render(){
