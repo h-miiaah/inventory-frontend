81f3830a1635abd753187d24531213d4c5d3ce26
h-miiaah <haim.miiaah5@gmail.com>
2021-02-28 15:25:23 -0500

    added all the imports needed to index.js (createStore, applyMiddleware, thunk, Provider).

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {createStore, applyMiddleware} from 'redux'; // createStore for the store, applyMiddleware is to incorporate thunk with our store, it comes from redux.
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux'; // any component we wrap in a Provider will have access to our redux store.
+
+
 import App from './App';
+
+// set up the store ("import {createStore} from 'redux'")
 
 ReactDOM.render(
     <App />,
