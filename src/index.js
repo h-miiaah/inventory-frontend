e957e44abdec58c9591a54a440b20cc327c84ed7
h-miiaah <haim.miiaah5@gmail.com>
2021-03-01 21:30:22 -0500

    boxReducer reducer function added to reducers folder, imported the boxReducer to index.js, and updated the store to include the boxReducer reducer function.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux'; // createStore for the store, applyMiddleware is to incorporate thunk with our store, it comes from redux.
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux'; // any component we wrap in a Provider will have access to our redux store.
-
+import boxReducer from './reducers/boxReducer'; // import the boxReducer reducer function.
 
 import App from './App';
 
@@ -12,7 +12,7 @@ import App from './App';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // gives us access to the redux dev tools.
 
 // set up the store ("import {createStore} from 'redux'")
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+let store = createStore(boxReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store} >
---
+++ src/reducers/boxReducer.js
@@ -0,0 +1,3 @@
+export default function boxReducer(state ={boxes: [], action}) {
+    return state;
+}
