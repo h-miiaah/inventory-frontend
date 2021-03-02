f1239c0eb9a530ad0682f7a23d5b359edcc72c30
h-miiaah <haim.miiaah5@gmail.com>
2021-03-01 21:14:31 -0500

    set up the store, added access to the redux dev tools with the composeEnhancers variable.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {createStore, applyMiddleware} from 'redux'; // createStore for the store, applyMiddleware is to incorporate thunk with our store, it comes from redux.
+import {createStore, applyMiddleware, compose} from 'redux'; // createStore for the store, applyMiddleware is to incorporate thunk with our store, it comes from redux.
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux'; // any component we wrap in a Provider will have access to our redux store.
 
 
 import App from './App';
 
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // gives us access to the redux dev tools.
+
 // set up the store ("import {createStore} from 'redux'")
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <Provider >
+  <Provider store={store} >
     <App />
   </Provider>,
   document.getElementById('root')
