fdabe3d3238652a7cda41a4bc76b07386438ef65
h-miiaah <haim.miiaah5@gmail.com>
2021-09-14 21:50:35 -0400

    adjusted ItemsContainer so the div element works

--- src/containers/ItemsContainer.js
+++ src/containers/ItemsContainer.js
@@ -1,12 +1,28 @@
+// import React from 'react';
+
+// class ItemsContainer extends React.Component {
+//     render(){
+//         return {
+//             <div>
+//                 ItemsContainer
+//             </div>
+//         }
+//     }
+// }
+
+// export default ItemsContainer
+
+
 import React from 'react';
 
 class ItemsContainer extends React.Component {
-    render(){
-        return {
+
+    render() {
+        return(
             <div>
-                ItemsContainer
+                ItemsComponent
             </div>
-        }
+        )
     }
 }
 
