aafdb47df97cd0906f3e01b0c87d40acd2b89df2
h-miiaah <haim.miiaah5@gmail.com>
2021-09-14 21:16:32 -0400

    added redirect to the Box js so we can redirect to the main boxes page if no box with that id exists

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,13 +1,16 @@
 // Show page for a box.
 import React from 'react'
+import {Redirect} from 'react-router-dom'
 
 const Box = (props) => {
-    console.log(props)
+    // console.log(props)
 
     let box = props.boxes[props.match.params.id - 1]
+    console.log(box)
 
     return (
         <li>
+            {box ? null : <Redirect to='/boxes'/>}
             {box ? box.name : null} - {box ? box.amount : null}
         </li>
     )
