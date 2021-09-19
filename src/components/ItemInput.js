09544b052d1bbd4cd31422405f526c0548dea2fb
h-miiaah <haim.miiaah5@gmail.com>
2021-09-19 13:58:22 -0400

    added the handleChange function and onChange to the inputs in the form

--- src/components/ItemInput.js
+++ src/components/ItemInput.js
@@ -13,12 +13,18 @@ class ItemInput extends React.Component {
        image_url: '' 
     }
 
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
     render(){
         return(
             <div>
                 <form>
                     <label>Add or Remove Item </label>
-                    <select>
+                    <select name="kind" value={this.state.kind} onChange={this.handleChange}>
                         <option>add</option>
                         <option>remove</option>
                     </select>
@@ -26,32 +32,32 @@ class ItemInput extends React.Component {
                     <br></br>
 
                     <label>Item Description: </label>
-                    <input type="text" name="description" value={this.state.description}></input>
+                    <input type="text" name="description" value={this.state.description} onChange={this.handleChange}></input>
 
                     <br></br>
                     
                     <label>Item Style: </label>
-                    <input type="text" name="style" value={this.state.style}></input>
+                    <input type="text" name="style" value={this.state.style} onChange={this.handleChange}></input>
 
                     <br></br>
                     
                     <label>Item Color: </label>
-                    <input type="text" name="color" value={this.state.color}></input>
+                    <input type="text" name="color" value={this.state.color} onChange={this.handleChange}></input>
 
                     <br></br>
                     
                     <label>Item Size: </label>
-                    <input type="text" name="size" value={this.state.size}></input>
+                    <input type="text" name="size" value={this.state.size} onChange={this.handleChange}></input>
 
                     <br></br>
                     
                     <label>Item Quantity: </label>
-                    <input type="text" name="quantity" value={this.state.quantity}></input>
+                    <input type="text" name="quantity" value={this.state.quantity} onChange={this.handleChange}></input>
 
                     <br></br>
 
                     <label>Item Image URL: </label>
-                    <input type="text" name="image_url" value={this.state.image_url}></input>
+                    <input type="text" name="image_url" value={this.state.image_url} onChange={this.handleChange}></input>
 
                     <br></br>
 
